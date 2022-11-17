e885863e346b782680f424892b190b6c70cfc444
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-17 06:53:46 +0300

    Kütüphanenin oluşturulması

---
+++ app/src/main/java/com/hozanbaydu/ktphaneoluturma/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hozanbaydu.ktphaneoluturma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
